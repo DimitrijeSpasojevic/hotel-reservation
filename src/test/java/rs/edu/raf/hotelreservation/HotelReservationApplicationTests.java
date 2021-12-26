e46434e955224b40ee498b305c82bb1203d4c7a7
DimitrijeSpasojevic <dspasojevic134@gmail.com>
2021-12-26 16:16:21 +0100

    Initial commit

---
+++ src/test/java/rs/edu/raf/hotelreservation/HotelReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.edu.raf.hotelreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelReservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
